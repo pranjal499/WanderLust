9ab026849243a964f836e7abbefbabbc8a096b78
pranjal499 <2006pranjalsingh@gmail.com>
2025-10-09 09:39:13 +0530

    new features

--- index.js
+++ index.js
@@ -6,6 +6,9 @@ const Listing = require('./models/listing.js');
 const path = require('path');
 const methodOverride = require('method-override');
 const ejsMate = require('ejs-mate');
+const wrapAsync = require('./utils/wrapAsync.js');
+const ExpressError = require('./utils/ExpressError.js');
+const {listingSchema} = require('./schema.js');
 
 // Mondo db url:
 const uri = 'mongodb+srv://Pranjal:Pranjal9826@cluster0.i8xujwe.mongodb.net/wanderlust?retryWrites=true&w=majority&appName=Cluster0';
@@ -41,46 +44,76 @@ app.get('/', (req, res) => {
     res.send("Working...");
 });
 
+// Validate listing function:
+const validateListing = (req, res, next) => {
+    let {error} = listingSchema.validate(req.body);
+    if (error) {
+        let errMsg = error.details.map((el) => el.message).join(',');
+        throw new ExpressError(400, errMsg);
+    }
+    else {
+        next();
+    }
+}
+
 // Listing rout:
-app.get('/listings', async (req, res) => {
+app.get('/listings', wrapAsync(async (req, res) => {
     const allListing = await Listing.find();
     res.render('listings/index.ejs', {allListing});
-});
+}));
 
 // Create rout:
 app.get('/listings/new', (req, res) => {
     res.render('listings/new.ejs');
 });
 
-app.post('/listings', async (req, res) => {
+app.post('/listings',validateListing, wrapAsync(async (req, res) => {
+    let result = listingSchema.validate(req.body.listing);
+    if (result.error) {
+        throw new ExpressError(400, result.error);
+    }
     const newListing = new Listing(req.body.listing);
     await newListing.save();
     res.redirect('/listings');
-});
+}));
 
 // Show rout:
-app.get('/listings/:id', async (req, res) => {
+app.get('/listings/:id', wrapAsync(async (req, res) => {
     let {id} = req.params;
     const item = await Listing.findById(id);
     res.render('listings/show.ejs', {item});
-});
+}));
 
 // Update rout:
-app.get('/listings/:id/edit', async (req, res) => {
+app.get('/listings/:id/edit', wrapAsync(async (req, res) => {
     let {id} = req.params
     const item = await Listing.findById(id);
     res.render('listings/edit.ejs', {item});
-});
+}));
 
-app.put('/listings/:id', async(req, res) => {
+app.put('/listings/:id', validateListing, wrapAsync(async(req, res) => {
+    if (!req.body.listing) {
+        throw new ExpressError(400, "Send valid data for listing...")
+    }
     let {id} = req.params;
     await Listing.findByIdAndUpdate(id, {...req.body.listing})
     res.redirect(`/listings/${id}`);
-});
+}));
 
 // Delete rout:
-app.delete('/listings/:id', async (req, res) => {
+app.delete('/listings/:id', wrapAsync(async (req, res) => {
     let {id} = req.params;
     await Listing.findByIdAndDelete(id);
     res.redirect('/listings');
+}));
+
+// Error handeling middleware:
+app.all('/{*all}', (req, res, next) => {
+    next(new ExpressError(404, 'Page not found!'));
+});
+
+app.use((err, req, res, next) => {
+    let {status = 500, message = 'Somethig went wrong!'} = err;
+    res.render('error.ejs', {message});
+    // res.status(status).send(message);
 });
--- models/listing.js
+++ models/listing.js
@@ -1,4 +1,5 @@
 // Rquire essentials:
+const { required } = require('joi');
 const mongoose = require('mongoose');
 const Schema = mongoose.Schema;
 
@@ -8,7 +9,10 @@ const listignSchema = new Schema({
         type: String,
         required: true
     },
-    description: String,
+    description: {
+        type: String,
+        required: true
+    },
     image: {
         url: {
             type: String,
@@ -17,9 +21,22 @@ const listignSchema = new Schema({
             set: (v) => v === "" ? "https://images.unsplash.com/photo-1519046904884-53103b34b206?q=80&w=1740&auto=format&fit=crop&ixlib=rb-4.1.0&ixid=M3wxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8fA%3D%3D" : v
         }
     },
-    price: Number,
-    location: String,
-    country: String
+    price: {
+        type: Number,
+        required: true
+    },
+    location: {
+        type: String,
+        require: true
+    },
+    country: {
+        type: String,
+        required: true
+    },
+    review: {
+        type: Schema.Types.ObjectId,
+        ref: "Review"
+    }
 });
 
 const Listing = mongoose.model('Listing', listignSchema);
